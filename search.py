418f2c475dab103082e5d36031af7ad4d87c8069
Nami Modarressi <s.namimodarressi@gmail.com>
2021-10-31 00:33:43 +0330

    bfs added

--- search.py
+++ search.py
@@ -71,7 +71,7 @@ def tinyMazeSearch(problem):
     s = Directions.SOUTH
     w = Directions.WEST
     return  [s, s, w, s, w, w, s, w]
-
+#----------------------------------------------
 def depthFirstSearch(problem):
     nodeStack = util.Stack()
     checked = []
@@ -93,6 +93,7 @@ def depthFirstSearch(problem):
                 for temp in checked:
                     if child[0] == temp:
                         isChecked = 1
+                        break
                         # we have checked the node
                 if isChecked == 0:
                     nodeStack.push(child[0])
@@ -102,59 +103,49 @@ def depthFirstSearch(problem):
                     isChecked = 0
     else:
         return None
-
+#----------------------------------------------
 def breadthFirstSearch(problem):
-    from util import Queue
-
-    # queueXY: ((x,y),[path]) #
-    queueXY = Queue()
-
-    visited = [] # Visited states
-    path = [] # Every state keeps it's path from the starting state
-
-    # Check if initial state is goal state #
-    if problem.isGoalState(problem.getStartState()):
-        return []
-
-    # Start from the beginning and find a solution, path is empty list #
-    queueXY.push((problem.getStartState(),[]))
-
-    while(True):
-
-        # Terminate condition: can't find solution #
-        if queueXY.isEmpty():
-            return []
-
-        # Get informations of current state #
-        xy,path = queueXY.pop() # Take position and path
-        visited.append(xy)
-
-        # Comment this and uncomment 179. This is only works for autograder
-        # In lectures we check if a state is a goal when we find successors
-
-        # Terminate condition: reach goal #
-        if problem.isGoalState(xy):
-            return path
-
-        # Get successors of current state #
-        succ = problem.getSuccessors(xy)
-
-        # Add new states in queue and fix their path #
-        if succ:
-            for item in succ:
-                if item[0] not in visited and item[0] not in (state[0] for state in queueXY.list):
-
-                    # Lectures code:
-                    # All impementations run in autograder and in comments i write
-                    # the proper code that i have been taught in lectures
-                    # if problem.isGoalState(item[0]):
-                    #   return path + [item[1]]
-
-                    newPath = path + [item[1]] # Calculate new path
-                    queueXY.push((item[0],newPath))
-
-
-
+    nodeQueue = util.Queue()
+    checked = []
+    nodes = set([])
+    startState = problem.getStartState()
+    nodeQueue.push((startState , []))
+    nodes.add(startState)
+    # pop , check , not goal -> push childNodes
+    while nodeQueue.isEmpty() == 0:
+        state ,actions = nodeQueue.pop()
+        nodes.discard(state)
+        if problem.isGoalState(state):
+            checked.append(state)
+            return actions
+        else:
+            checked.append(state)
+            childNodes = problem.getSuccessors(state)
+            isChecked = 0
+            isInNodes = 0
+            for child in childNodes:
+                for temp in checked:
+                    if child[0] == temp:
+                        isChecked = 1
+                        break
+                        # we have checked the node
+                if isChecked == 0 :
+                    for temp in nodes:
+                        if child[0] == temp:
+                            isInNodes = 1
+                            break
+                            # we have the node
+                    if isInNodes == 0:
+                        nodes.add(child[0])
+                        childActions = actions + [child[1]]
+                        nodeQueue.push((child[0],childActions))
+                    else:
+                        isInNodes = 0
+                else:
+                    isChecked = 0
+    else:
+        return None
+#----------------------------------------------
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
     "*** YOUR CODE HERE ***"
